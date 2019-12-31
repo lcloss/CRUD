4702f601bab4f1847572b4342a765895f734e638
Luciano Closs <lcloss@gmail.com>
2019-12-31 18:20:20 +0000

    Fix error on install when public/uploads folder already exists.

--- src/app/Console/Commands/Install.php
+++ src/app/Console/Commands/Install.php
@@ -58,4 +58,60 @@ class Install extends Command
         $this->progressBar->finish();
         $this->info(' Backpack installation finished.');
     }
+
+    /**
+     * Run a SSH command.
+     *
+     * @param string $command      The SSH command that needs to be run
+     * @param bool   $beforeNotice Information for the user before the command is run
+     * @param bool   $afterNotice  Information for the user after the command is run
+     *
+     * @return mixed Command-line output
+     */
+    public function executeProcess($command, $beforeNotice = false, $afterNotice = false)
+    {
+        if ( empty($command) ) return;
+        
+        $this->echo('info', $beforeNotice ? ' '.$beforeNotice : $command);
+
+        $process = new Process($command, null, null, null, $this->option('timeout'));
+        $process->run(function ($type, $buffer) {
+            if (Process::ERR === $type) {
+                $this->echo('comment', $buffer);
+            } else {
+                $this->echo('line', $buffer);
+            }
+        });
+
+        // executes after the command finishes
+        if (! $process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        if ($this->progressBar) {
+            $this->progressBar->advance();
+        }
+
+        if ($afterNotice) {
+            $this->echo('info', $afterNotice);
+        }
+    }
+
+    /**
+     * Write text to the screen for the user to see.
+     *
+     * @param string $type    line, info, comment, question, error
+     * @param string $content
+     */
+    public function echo($type, $content)
+    {
+        if ($this->option('debug') == false) {
+            return;
+        }
+
+        // skip empty lines
+        if (trim($content)) {
+            $this->{$type}($content);
+        }
+    }
 }
